b902b542a549823d3057e0b1243e05fd0a8cf609
cmsjulio <cms.julio1@gmail.com>
2023-04-11 15:34:56 -0300

    refactor(Task.ts): :hammer: refatora tipagem e nomenclatura do modelo Task, referente ao jogador

--- src/app/Task.ts
+++ src/app/Task.ts
@@ -1,6 +1,6 @@
-export interface Task{
-    id?: number;
-    text: string;
-    day: string;
-    reminder: boolean;
+export interface Task {
+  id?: number;
+  nome: string;
+  nivel: number;
+  reminder: boolean;
 }
