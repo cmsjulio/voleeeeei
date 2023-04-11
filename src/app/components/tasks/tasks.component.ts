c3faf5a5454167b7317bd1704144a1c0d6c22b84
cmsjulio <cms.julio1@gmail.com>
2023-04-11 15:42:02 -0300

    feat(tasks.component.ts): :sparkles: implementa ordenação de jogadores por nível, implementa função realizarSorteio, chamando classe Sorteio

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,35 +1,50 @@
+import { Sorteio } from './../../sorteio';
 import { Component, OnInit } from '@angular/core';
-import { Task } from 'src/app/Task';
 import { TaskService } from 'src/app/services/task.service';
+import { Task } from 'src/app/Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent implements OnInit {
   tasks: Task[] = [];
+  tasksParaSorteio: Task[] = [];
 
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    this.taskService.getTasks().subscribe((tasks)=>
-    this.tasks = tasks);
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks.sort((a, b) => (a.nivel < b.nivel ? 1 : -1));
+      this.tasksParaSorteio = this.tasks.filter((t) => t.reminder == true);
+    });
   }
 
-  deleteTask(task: Task){
-    this.taskService.deleteTask(task).subscribe(()=>
-    this.tasks = this.tasks.filter(t=>t.id!== task.id));
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => {
+      this.tasks = this.tasks.filter((t) => t.id !== task.id);
+      this.tasks = this.tasks.sort((a, b) => (a.nivel < b.nivel ? 1 : -1));
+    });
   }
 
-  toggleReminder(task: Task){
+  toggleReminder(task: Task) {
     task.reminder = !task.reminder;
-    this.taskService.updateTaskReminder(task).subscribe()
+    this.tasksParaSorteio = this.tasks.filter((t) => t.reminder == true);
+    this.tasks = this.tasks.sort((a, b) => (a.nivel < b.nivel ? 1 : -1));
+    this.taskService.updateTaskReminder(task).subscribe();
   }
 
-  addTask(task: Task){
-    this.taskService.addTask(task).subscribe((task)=>
-    this.tasks.push(task));
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => {
+      this.tasks.push(task);
+      this.tasksParaSorteio = this.tasks.filter((t) => t.reminder == true);
+      this.tasks = this.tasks.sort((a, b) => (a.nivel < b.nivel ? 1 : -1));
+    });
   }
 
+  realizarSorteio(): void {
+    const teste = new Sorteio(this.tasksParaSorteio);
+    teste.teste();
+  }
 }
